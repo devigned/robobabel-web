097af6444074bd2249b9de41db2e3393eea32b12
David Justice <david@devigned.com>
2017-01-11 13:43:30 -0800

    working with jsonapi and solid auth

--- app/models/health.js
+++ app/models/health.js
@@ -1,10 +1,7 @@
 import DS from 'ember-data';
-import Inflector from 'ember-inflector';
-
-const inflector = Inflector.inflector;
-
-inflector.uncountable('health');
 
 export default DS.Model.extend({
-  status: DS.attr('string')
+  status: DS.attr('string'),
+  name: DS.attr('string'),
+  updated_at: DS.attr('date')
 });
